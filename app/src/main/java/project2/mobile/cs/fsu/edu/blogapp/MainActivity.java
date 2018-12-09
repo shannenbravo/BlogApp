9073351abd599f691280ee3ea9376f8944853857
briabostick <bb16x>
2018-12-09 13:08:27 -0500

    started activities for blog posts

--- app/src/main/java/project2/mobile/cs/fsu/edu/blogapp/MainActivity.java
+++ app/src/main/java/project2/mobile/cs/fsu/edu/blogapp/MainActivity.java
@@ -1,13 +1,95 @@
 package project2.mobile.cs.fsu.edu.blogapp;
 
+import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+//    private FirebaseAuth mAuth;
+
+    private FloatingActionButton addPostButton;
+
+    private BottomNavigationView mainbottomNav;
+
+//    private HomeFragment homeFragment;
+//    private NotificationFragment notificationFragment;
+//    private AccountFragment accountFragment;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addPostButton = findViewById(R.id.addPost_button);
+        addPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent newPostIntent = new Intent(MainActivity.this, NewPostActivity.class);
+                startActivity(newPostIntent);
+
+            }
+        });
+
+    }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+//        if (currentUser == null) {
+//
+//            sendToLogin();
+//        }
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.logoutOption:
+                logOut();
+                return true;
+
+
+            default:
+                return false;
+
+
+        }
+
+    }
+
+    private void logOut() {
+
+        sendToLogin();
+    }
+
+
+    private void sendToLogin() {
+
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+
     }
 }
