008e2b454fdaf25348837d612761aa3ca80d9c78
shannenbravo <shannenbravo@gmail.com>
2018-12-12 20:01:17 -0500

    added alert

--- app/src/main/java/project2/mobile/cs/fsu/edu/blogapp/Register.java
+++ app/src/main/java/project2/mobile/cs/fsu/edu/blogapp/Register.java
@@ -1,7 +1,9 @@
 package project2.mobile.cs.fsu.edu.blogapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void showAlert(String errMessage){
+        AlertDialog.Builder formNotComplete = new AlertDialog.Builder(Register.this);
+        formNotComplete.setMessage(errMessage).setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        AlertDialog alert = formNotComplete.create();
+        alert.setTitle("Alert!");
+        alert.show();
     }
 }
