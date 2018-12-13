2aa07fb7cd1ced5aa3948bef4bb45915a11acb4a
seane <seanebanks1@gmail.com>
2018-12-13 14:57:46 -0500

    Routing to Dashboard works now

--- app/src/main/java/project2/mobile/cs/fsu/edu/blogapp/Dashboard.java
+++ app/src/main/java/project2/mobile/cs/fsu/edu/blogapp/Dashboard.java
@@ -10,27 +10,34 @@ public class Dashboard extends AppCompatActivity {
 
     LinearLayout write;
     LinearLayout readStuff;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        Bundle extras = getIntent().getExtras();
+        if(extras == null) {
+            return;
+        }
+        user = extras.getParcelable(MainActivity.PASS_USER);
+
         write = findViewById(R.id.writePost);
         write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( Dashboard.this, NewPostActivity.class);
+                intent.putExtra(MainActivity.PASS_USER, user);
                 startActivity(intent);
             }
         });
         readStuff = findViewById(R.id.readApost);
-
         readStuff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent( Dashboard.this, MainActivity.class);
+                intent.putExtra(MainActivity.PASS_USER, user);
                 startActivity(intent);
 
             }
